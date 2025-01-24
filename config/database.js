e8290ed26631e46af5d4c8a31b033c0e808c775f
amit1308rupee <amit.kumar03@rupee.com>
2025-01-24 13:06:57 +0530

    resolve database connection when it closed

--- config/database.js
+++ config/database.js
@@ -1,21 +1,29 @@
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 3306
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10, // Adjust the limit based on your needs
+    queueLimit: 0
 });
 
-(async () => {
+async function handleDisconnect() {
     try {
         const connection = await pool.getConnection();
-        console.log('MYSQL connection successfully');
+        console.log('MySQL connection successfully');
         connection.release();
     } catch (err) {
         console.error('Error connecting to the database:', err.message);
+        handleDisconnect(); // Reconnect if the connection is lost
+        setTimeout(handleDisconnect, 2000); // Reconnect after 2 seconds
     }
-})();
+}
+
+handleDisconnect();
 
 module.exports = pool;
